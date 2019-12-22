5b9f2e07df18cb2e84b53df8eed93de3d9e59e67
doer <499361328@qq.com>
2019-12-22 23:02:03 +0800

    mm-uni-app: 1 项目介绍 2 环境搭建和项目创建 3 App.vue引入全局公共样式 4 底部导航开发 5 UI基础库封装

---
+++ mm-uni-app/App.vue
@@ -0,0 +1,27 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/* 官方ui库 */
+	@import url("/common/uni.css");
+	/* 第三方动画库 */
+	@import url("/common/animate.css");
+	/* 自定义图标库 */
+	@import url("/common/icon.css");
+	/* UI基础库 */
+	@import url("/common/zcm-main.css");
+	/* 公共样式 */
+	@import url("/common/common.css");
+</style>
