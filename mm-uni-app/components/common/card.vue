1cefbe11a39329f1bb95803c39f1f12edac2c737
doer <499361328@qq.com>
2020-02-19 20:49:11 +0800

    mm-uni-app: 9 搜索页开发

--- mm-uni-app/components/common/card.vue
+++ mm-uni-app/components/common/card.vue
@@ -1,17 +1,18 @@
 <template>
-	<view class="uni-card">
-		<!-- 标题 -->
-		<view v-if="showhead" class="p-2 border-bottom main-border-color">
+	<view class="card" :style="cardStyle">
+		<!-- head -->
+		<view v-if="showhead" class="p-2 main-border-color"
+		:class="getHeadClass">
 			<slot name="title">
-				<text class="font-md font-weight">{{headTile}}</text>
+				<text v-if="headTitle" class="font-md"
+				:class="headTitleWeight?'font-weight':''">{{headTitle}}</text>
 			</slot>
 		</view>
 		<!-- body -->
-		<view>
-			<image v-if="bodyCover" :src="bodyCover"
-			mode="widthFix"
-			></image>
-			<slot name="body"></slot>
+		<view :class="getBodyClass" :style="bodyStyle">
+			<image v-if="bodyCover" :src="bodyCover" 
+			mode="widthFix"></image>
+			<slot />
 		</view>
 	</view>
 </template>
@@ -19,18 +20,48 @@
 <script>
 	export default {
 		props:{
+			bodyStyle:String,
 			// 头部标题
-			headTile:String,
+			headTitle:String,
 			// 封面图
 			bodyCover:String,
 			// 是否显示头部
 			showhead:{
 				type:Boolean,
 				default:true
+			},
+			// 是否显示下边线
+			headBorderBottom:{
+				type:Boolean,
+				default:true
+			},
+			// 标题是否加粗
+			headTitleWeight:{
+				type:Boolean,
+				default:true
+			},
+			// 是否给body加padding
+			bodyPadding:{
+				type:Boolean,
+				default:false
+			},
+			cardStyle:{
+				type:String,
+				default:""
 			}
-		}
+		},
+		computed: {
+			getHeadClass() {
+				let BorderBottom = this.headBorderBottom ? 'border-bottom':''
+				return `${BorderBottom}`
+			},
+			getBodyClass(){
+				let padding = this.bodyPadding ? 'p-2' : ''
+				return `${padding}`
+			}
+		},
 	}
 </script>
-	
+
 <style>
 </style>
---
+++ mm-uni-app/components/common/card1.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="uni-card">
+		<!-- 标题 -->
+		<view v-if="showhead" class="p-2 main-border-color" :class="getHeadClass">
+			<slot name="title">
+				<text class="font-md font-weight">{{headTitle}}</text>
+			</slot>
+		</view>
+		<!-- body -->
+		<view :class="getBodyClass">
+			<image v-if="bodyCover" :src="bodyCover"
+			mode="widthFix"
+			></image>
+			<slot name="body"></slot>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props:{
+			// 头部标题
+			headTitle:String,
+			// 封面图
+			bodyCover:String,
+			// 是否显示头部
+			showhead:{
+				type:Boolean,
+				default:true
+			},
+			// 是否显示下边线
+			headBorderBottom:{
+				type:Boolean,
+				default:true
+			},
+			// 是否给body加padding
+			bodyPadding:{
+				type:Boolean,
+				default:false
+			}
+		},
+		computed: {
+			getHeadClass() {
+				return {'border-bottom': this.headBorderBottom}
+			},
+			getBodyClass() {
+				return {'p-2': this.bodyPadding}
+			}
+		}
+	}
+</script>
+	
+<style>
+</style>
